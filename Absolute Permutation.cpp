f26209cd1537d8c92e6331859ba4253bda1a28ea
ragavendrakumarranjithkumar <rrk350@nyu.edu>
2018-12-01 12:34:55 -0500

    Absolute Permutation

---
+++ Absolute Permutation.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the absolutePermutation function below.
+vector<int> absolutePermutation(int n, int k) {
+  vector<int> ans;
+  if (k == 0) {
+    for (int i = 0; i < n; i++) {
+      ans.push_back(i + 1);
+    }
+    return ans;
+  }
+  if(n%(2*k)!=0)
+  {
+      ans.push_back(-1);
+      return ans;
+  }
+  else
+  {
+    for(int i=1;i<=n/(2*k);i++){
+        for(int j1 = 1+(((i*2)-2)*k); j1<=((i*2)-1)*k;j1++){
+            ans.push_back(j1+k);
+        }
+        for (int j2 = (((i * 2) - 1) * k) + 1; j2 <= (2 * i * k); j2++) {
+          ans.push_back(j2 - k);
+        }
+    }
+    return ans;
+  }
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string nk_temp;
+        getline(cin, nk_temp);
+
+        vector<string> nk = split_string(nk_temp);
+
+        int n = stoi(nk[0]);
+
+        int k = stoi(nk[1]);
+
+        vector<int> result = absolutePermutation(n, k);
+
+        for (int i = 0; i < result.size(); i++) {
+            fout << result[i];
+
+            if (i != result.size() - 1) {
+                fout << " ";
+            }
+        }
+
+        fout << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
