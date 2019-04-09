bc2e7e2fadf240e5da8601aeecd8446424f7ed25
ragavendrakumarranjithkumar <rrk350@nyu.edu>
2019-04-08 23:59:56 -0400

    freqsort

---
+++ freqsort.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<vector>
+#include<map>
+using namespace std;
+void customSort(vector<int> arr) {
+    map<int, int> tally;
+    for(const auto& elem : arr){
+        if(tally.find(elem)!=tally.end())
+            tally[elem]++;
+        else{
+            tally[elem]=1;
+        }
+    }
+    map<int, vector<int>> invert_tally;
+    for(auto it=tally.begin(); it!=tally.end(); it++)
+    {
+      if (invert_tally.find(it->second) != invert_tally.end()){
+        //vector<int> init;
+        for (int j = 0; j < it->second; j++)
+          invert_tally[it->second].push_back(it->first);
+        //invert_tally[it->second].push_back(init);
+      }
+        //invert_tally[it->second].push_back(it->first);
+      else {
+          
+          vector<int> init;
+          for(int j=0; j<it->second; j++)
+            init.push_back(it->first);
+        invert_tally[it->second] = init;
+      }
+    }
+    for(auto it=invert_tally.begin(); it!=invert_tally.end(); it++){
+        cout<<it->second[0]<<" ";
+    }
+    cout<<endl;
+}
+int main(){
+    vector<int> a = {1, 2, 2, 2, 7, 7, 3, 3, 93};
+    customSort(a);
+}
