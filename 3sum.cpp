46cf3a9400b4e5c08f264b03e2a2e0b38d23a868
ragavendrakumarranjithkumar <rrk350@nyu.edu>
2019-07-19 23:28:53 -0700

    using 2 sum to find 3 sum

--- 3sum.cpp
+++ 3sum.cpp
@@ -1,39 +1,41 @@
+using pairint = pair<int, int>;
 class Solution {
-    bool binary_search(const vector<int>& nums, const int n){
-        int l = 0;
-        int r = nums.size()-1;
-        while(l<=r){
-            int c = (l+r)/2;
-            cout<<"l = "<<l<<"; r = "<<r<<"; c = "<<c<<endl;
-            cout<<"nums[c] = "<<nums[c]<<endl; 
-            if(nums[c]==n)
+    bool twoSum(int t, const vector<int>& a, pairint& p){
+        cout<<"target: "<<t<<endl;
+        int i=0;
+        int j=a.size()-1;
+        while(i<j){
+            cout<<"a[i] = "<<a[i]<<"; a[j] = "<<a[j]<<endl;
+            if(a[i]+a[j]>t){
+                j--;
+            }
+            else if(a[i]+a[j]<t){
+                i++;
+            }
+            else{
+                p.first = a[i];
+                p.second = a[j];
                 return true;
-            else if(nums[c]>n)
-                r=c-1;
-            else if(nums[c]<n)
-                l=c+1;
+            }
         }
         return false;
     }
-        
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<int> a = {2 ,3, 4, 5, 6, 7};
-        cout<<"binary_search(a,4) = "<<binary_search(a,4)<<endl;
-        cout<<"binary_search(a,8) = "<<binary_search(a,8)<<endl;
-        cout<<"binary_search(a,2) = "<<binary_search(a,2)<<endl;
-        cout<<"binary_search(a,7) = "<<binary_search(a,7)<<endl;
-        
-        vector<int> b = {2};
-        cout<<"binary_search(b,2) = "<<binary_search(b,2)<<endl;
-        
-        vector<int> c = {};
-        cout<<"binary_search(c,1) = "<<binary_search(c,1)<<endl;
-        
-        vector<int> d = {2, 3};
-        cout<<"binary_search(d,2) = "<<binary_search(d,2)<<endl;
-        cout<<"binary_search(d,3) = "<<binary_search(d,3)<<endl;
-        cout<<"binary_search(d,0) = "<<binary_search(d,0)<<endl;
-        return vector<vector<int>>();
+       vector<vector<int>> res;
+       sort(nums.begin(), nums.end());
+       for(int i =0; i<nums.size();i++){
+           int t = -nums[i];
+           vector<int> r = vector(nums.begin()+i+1, nums.end());
+           pairint p = pair<int, int>();
+           if(twoSum(t, r, p)){
+               cout<<"twoSum("<<t<<")"<<"returned true";
+               vector<int> a = {-t, p.first, p.second};
+               res.push_back(a);
+           }
+           else
+               cout<<"twoSum("<<t<<")"<<"returned false";
+       } 
+       return res;
     }
 };
