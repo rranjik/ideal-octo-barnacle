96aa6e9f599d2327ca5ad5b0fadeb057e20b6fee
ragavendrakumarranjithkumar <rrk350@nyu.edu>
2019-08-24 23:06:13 -0400

    LargestNumber

---
+++ LargestNumber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), [](int a, int b){
+            return to_string(a)+to_string(b) > to_string(b)+to_string(a);
+        });
+        string res;
+        for(int i = 0; i<nums.size(); i++)
+            res+=to_string(nums[i]);
+        return res[0]=='0'?"0":res;
+    }
+};
