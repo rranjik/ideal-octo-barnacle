5286e1f17f1f72e30a875b097f49560062f1747b
ragavendrakumarranjithkumar <rrk350@nyu.edu>
2020-03-29 22:36:04 -0400

    300-LongestIncreasingSubsequence

---
+++ 300-LongestIncreasingSubsequence.cpp
@@ -0,0 +1,36 @@
+class coor{public:int i; int l;coor(int _i, int _l){i = _i; l = _l;}
+          bool operator==(const coor& o)const{return i==o.i&&l==o.l;}};
+namespace std{template<>struct hash<coor>{size_t operator()(const coor& c)const
+{ return hash<int>()(c.i)^(hash<int>()(c.l)<<1);}};}
+class Solution {
+public:
+    vector<int> n;
+    unordered_map<coor, int> c;
+    int dp(int i, int l){
+        auto cr = coor(i,l);
+        //if(c.find(coor(i,l))!=c.end()) return c[i];
+        if(c.find(cr)!=c.end()) return c[cr];
+        if(i<0) {c[cr] = 0;return 0;}
+        int res = 0;
+        int w, wo;
+        w = wo = 0;
+        wo = dp(i-1, l);
+        if((l<n.size()&&n[i]<n[l])||l>=n.size()) {w = dp(i-1, i)+1;}
+        c[cr] = max(w, wo);
+        return c[cr];
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> res(nums.size(), 1);
+        for(int j = 1; j<nums.size(); j++){
+            for(int i = 0; i<j; i++){
+                if(nums[i]<nums[j]) 
+                    res[j] = max(res[j], res[i]+1);
+            }
+        }
+        int m = 0;
+        for(int i = 0; i<nums.size(); i++){
+            if(m<res[i]) m = res[i];
+        }
+        return m;
+    }
+};
