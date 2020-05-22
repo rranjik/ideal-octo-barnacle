b3e5fc7a2b3f68c08041c9f4c42a2c26cc9d9a92
ragavendrakumarranjithkumar <rrk350@nyu.edu>
2020-05-22 16:15:50 -0400

    add comments

--- 1038-BinarySearchTreeToGreaterSumTree.cpp
+++ 1038-BinarySearchTreeToGreaterSumTree.cpp
@@ -9,15 +9,20 @@
  */
 class Solution {
 public:
-    
+    //rs - running sum 
     TreeNode* traverse(TreeNode* root, int& rs){
         if(!root) return root;
         //cout<<"root.val = "<< root->val<<" rs = "<<rs<<endl;
+	//everything to right is greater than root
+	//so add them first
         traverse(root->right, rs);
         //cout<<"after right root.val = "<< root->val<<" rs = "<<rs<<endl;
+	//include this node, too
         rs+= root->val;
         root->val = rs;
         //cout<<"after self root.val = "<< root->val<<" rs = "<<rs<<endl;
+	//everything in the parent, and to the right of the parent will
+	//be greater, and we can go left now
         traverse(root->left, rs);
         //cout<<"after left root.val = "<< root->val<<" rs = "<<rs<<endl;
         return root;
