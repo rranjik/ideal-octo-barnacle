983e5fc7ceb16d1695a08c7d339184bd66effc3a
ragavendrakumarranjithkumar <rrk350@nyu.edu>
2020-07-04 11:22:05 -0700

    461-HammingDistance

---
+++ 461-HammingDistance.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        bitset<32> a(x);
+        bitset<32> b(y);
+        int res =0;
+        for(int i = 0; i<32; i++){
+            if(a[i]!=b[i]){
+                res++;
+            }
+        }
+        return res;
+    }
+};
