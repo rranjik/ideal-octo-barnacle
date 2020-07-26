1d84dff1fdc80269783505532d56c232db5af987
ragavendrakumarranjithkumar <rrk350@nyu.edu>
2020-07-26 11:59:57 -0700

    1313-DecompressRun-LengthEncodedList

---
+++ 1313-DecompressRun-LengthEncodedList.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        vector<int> res;
+        for(int i = 0; i<nums.size(); i+=2){
+            for(int f = 1; f<=nums[i]; f++){
+                res.push_back(nums[i+1]);
+            }
+        }
+        return res;
+    }
+};
