2371ad454931db4b8c645c4c3cfe2a563037e721
rranjik <43015276+rranjik@users.noreply.github.com>
2021-10-15 23:06:19 -0700

    1443-MinimumTimeToCollectAllApplesInATree.cpp

--- 1443-MinimumTimeToCollectAllApplesInATree.cpp
+++ 1443-MinimumTimeToCollectAllApplesInATree.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
-    bool stoneGameIX(vector<int>& stones) {
-        vector<int> c(3);
-        for(const auto& a : stones){
-            c[a%3]++;
+    vector<vector<int>> adjl;
+    bool dfs(int src, int& res){
+        bool subTreeHasApples = false;
+        for(const auto& c : adjl[src]){
+            auto thisChildHasApple = dfs(c, res);
+            if(thisChildHasApple) res+=2;
+            subTreeHasApples|= thisChildHasApple;
         }
-        if(min(c[1],c[2])==0){
-            return max(c[1], c[2])>2&&(c[0]%2!=0);
+        return subTreeHasApples|has[src];
+    }
+    vector<bool> has;
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
+        has = hasApple;
+        adjl = vector<vector<int>>(edges.size()+1);
+        for(int i = 0; i<edges.size(); i++){
+            adjl[edges[i][0]].push_back(edges[i][1]);
         }
-        return abs(c[1]-c[2])>2||c[0]%2==0;
+        int i = 0;
+        for(const auto& children : adjl){
+            cout<<"I'm "<<i<<". I can reach ";
+            for(const auto& c : children) cout<<c<<" ";
+            cout<<endl;
+        }
+        int res{};
+        dfs(0, res);
+        return res;
     }
 };
