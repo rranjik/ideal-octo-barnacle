82af0dd3eb77fbf51ed253e4a89e324a5205a7f8
rranjik <43015276+rranjik@users.noreply.github.com>
2024-02-06 22:11:30 -0800

    Feb 6

--- 50-Pow(x-n).cpp
+++ 50-Pow(x-n).cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    double help(double x, long long n){
+    unordered_map<int, double> c;
+    double posPow(double x, int n){
+        if(c.find(n)!=c.end()) return c[n];
         if(n==0) return 1;
-        if(n%2){
-            return x*help(x*x, (n-1)/2);
-        }else {
-            return help(x*x, n/2);
-        }
+        if(n==1) return x;
+        if(n%2) return c[n] = posPow(x, n/2)*posPow(x, n/2)*x;
+        return c[n] = posPow(x, n/2)*posPow(x, n/2);
     }
     double myPow(double x, int n) {
-        auto p = help(x, abs((long long)n));
-        if(n<0) return 1/p;
-        else return p;
+        if(n<0) {
+            //cout<<posPow(x, abs(n))<<endl;
+            return 1.0/posPow(x, abs(n));
+        }
+        //cout<<posPow(x, n)<<endl;
+        return posPow(x, n);
     }
 };
