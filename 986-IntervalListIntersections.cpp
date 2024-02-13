c55b6dfdef9aa7864209017b2152a3a302865b6e
rranjik <43015276+rranjik@users.noreply.github.com>
2024-02-12 20:25:42 -0800

    Feb 12

--- 986-IntervalListIntersections.cpp
+++ 986-IntervalListIntersections.cpp
@@ -1,44 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& a, 
+        vector<vector<int>>& b) {
+        int i = 0; int j = 0;
         vector<vector<int>> res;
-        int i = 0;
-        int j = 0;
-        res.push_back({-1, 0});
-        while(i<A.size()&&j<B.size()){
-            if(A[i][0]<=B[j][0]){
-                if(B[j][0]<=A[i][1]){
-                    vector<int> r = {B[j][0], min(B[j][1], A[i][1])};
+        res.push_back({-1, -2});
+        while(i<a.size()&&j<b.size()){
+            if(a[i][0]<=b[j][0]){
+                if(b[j][0]<=a[i][1]){
+                    vector<int> r = {b[j][0], min(b[j][1], a[i][1])};
                     if(res.back()[1]==r[0]){
-                        res.back()[1] = min(B[j][1], A[i][1]);
-                    }else {
-                        res.push_back(r);
-                    }
+                        res.back()[1] = r[1];
+                    }else res.push_back(r);
                 }
-                if(B[j][1]<=A[i][1]) j++;
-                else i++;
-            }else{
-                if(A[i][0]<=B[j][1]){
-                    vector<int> r = {A[i][0], min(A[i][1], B[j][1])};
-                    if(res.back()[1]==r[0]){
-                        res.back()[1] = min(A[i][1], B[j][1]);
-                    }else {
-                        res.push_back(r);
-                    }
-                }
-                if(A[i][1]<=B[j][1]) i++;
+                if(a[i][1]<b[j][1]) i++;
                 else j++;
             }
-            //for(const auto& i : res){
-            //    cout<<"["<<i[0]<<", "<<i[1]<<"]"<<" ";
-            //}
-            //cout<<endl;
+            else if(b[j][0]<=a[i][0]){
+                if(a[i][0]<=b[j][1]){
+                    vector<int> r = {a[i][0], min(b[j][1], a[i][1])};
+                    if(res.back()[1]==r[0]){
+                        res.back()[1] = r[1];
+                    }else res.push_back(r);
+                }
+                if(a[i][1]<b[j][1]) i++;
+                else j++;
+            }
         }
-        if(res[0][1] == 0){
-            res.erase(res.begin());
-        }else {
-            res[0][0] = 0;
-        }
+        res.erase(res.begin());
         return res;
     }
 };
