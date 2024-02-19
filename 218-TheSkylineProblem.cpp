c726e47ca3a545a27e4b42c2e059ca716a748f5a
rranjik <43015276+rranjik@users.noreply.github.com>
2024-02-18 16:08:39 -0800

    Feb 18

--- 218-TheSkylineProblem.cpp
+++ 218-TheSkylineProblem.cpp
@@ -1,62 +1,95 @@
 class Solution {
 public:
-    using pii = pair<int, int>;
-    //critical point
     struct cpoint{
-        bool isstart;
         int x;
-        int y;
-        cpoint(int _x, int _y, bool _a):x(_x), y(_y), isstart(_a){};
-        bool operator<(const cpoint& p)const{
-            //is the x coor collides
-            if(x==p.x){
-                //if one is a start and the other is an end
-                //start comes first
-                if(isstart^p.isstart){
-                    return isstart>p.isstart;
-                }else{
-                    if(isstart){
-                        //if both are starts
-                        //taller building comes first
-                        return y>p.y;
-                    }else{
-                        //if both are ends
-                        //shorter building is first
-                        return y<p.y;
-                    }
+        int h;
+        bool start;
+        //cpoint(const cpoint& c){this->x = c.x; this->h = c.h; this->start =c.start;}
+        cpoint(int _x, int _h, bool _start):x(_x), h(_h), start(_start){}
+        bool operator<(const cpoint& other){
+            if(x==other.x){
+                if(start^other.start){//only one is start
+                    //start precedes end
+                    /*
+
+                     |--------|--------|
+                     |        |        |
+                     |   a    |   b    |
+                     |        |        |
+                    _______________________
+                    b before a 
+                    so that we can maintain continuity
+                    */
+                    return start>other.start;
                 }
+                if(start){
+                    //this is start and so other is also start
+                    //you come before the other if you're taller
+                    /*
+                    both a and b are start
+                     __________
+                    |          |
+                    |----\  a  |
+                    |    |     |
+                    |  b |     |
+                    |    |     |
+                    _______________
+                    a before b
+
+                    so that a can overshadow
+                    */
+                    return h>other.h;
+                }
+                    //this is end and so other is also end
+                    //you come before the other if you're shorter
+                    /*
+                    both a and b are end
+                     __________
+                    |          |
+                    | a  /-----|
+                    |    |     |
+                    |    |  b  |
+                    |    |     |
+                    _______________
+                    b before a
+                    
+                    so that a can overshadow
+                    */
+                return h<other.h;
             }
-            return x<p.x;
+            return x<other.x;
         }
     };
     vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
-        map<int, int> prioq;
-        prioq.insert({0, 1});
         vector<cpoint> cpoints;
-        for(const auto& b : buildings){
-            cpoints.push_back(cpoint(b[0], b[2], true));
-            cpoints.push_back(cpoint(b[1], b[2], false));
+        for(int i = 0; i<buildings.size(); i++){
+            cpoints.push_back({buildings[i][0], buildings[i][2], true});
+            cpoints.push_back({buildings[i][1], buildings[i][2], false});
         }
-        sort(begin(cpoints), end(cpoints));
-        int prevmax = 0;
+        sort(cpoints.begin(), cpoints.end());
+        map<int, int> m; //heights -> num of cpoints with that height
+        m.insert({0, 1}); //there's a degenerate building of height 0 always
+        //you can also think of it as the ground
+        auto prevmax = 0;
         vector<vector<int>> res;
-        for(const auto& c : cpoints){
-            if(c.isstart){
-                prioq[c.y]++;
-                auto currmax = rbegin(prioq)->first;
-                if(currmax>prevmax){
-                    res.push_back({c.x, currmax});
-                    prevmax = currmax;
+        for(const auto& cp : cpoints){
+            //cout<<"x = "<<cp.x
+            //<<" h = "<<cp.h
+            //<<" start = "<<(cp.start?"true":"false")<<endl;
+            if(cp.start){
+                m[cp.h]++;
+                auto curmax = m.rbegin()->first;
+                if(prevmax<curmax){
+                    res.push_back({cp.x, curmax});
+                    prevmax = curmax;
                 }
             }else{
-                prioq[c.y]--;
-                if(!prioq[c.y]){
-                    prioq.erase(prioq.find(c.y));
-                }
-                auto currmax = rbegin(prioq)->first;
-                if(currmax<prevmax){
-                    res.push_back({c.x, currmax});
-                    prevmax = currmax;
+                m[cp.h]--;
+                if(!m[cp.h]) m.erase(m.find(cp.h));
+                auto curmax = m.rbegin()->first;
+                if(prevmax>curmax){
+                    res.push_back({cp.x, curmax});
+                    prevmax = curmax;
                 }
             }
         }
