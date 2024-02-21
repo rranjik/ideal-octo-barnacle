1c22f7ec233d08bb953d53583f0a634067b22fe1
rranjik <43015276+rranjik@users.noreply.github.com>
2024-02-21 15:57:09 -0800

    Feb 21

--- 76-MinimumWindowSubstring.cpp
+++ 76-MinimumWindowSubstring.cpp
@@ -1,45 +1,42 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char, int> m;
-        int count = 0;
-        for(const auto& c : t){
-            count++;
-            if(m.find(c)!=m.end()) m[c]++;
-            else m[c] = 1;
-        }
-        int l = 0, r = 0, mini = numeric_limits<int>::max(), minl = 0, minr = 0;
-        while(r<s.size()){
-            //cout<<"r = "<<r<<endl;
-            //if present decrement count 
-            if(m.find(s[r])!=m.end()){
-                m[s[r]]--;
-                //cout<<"found "<<s[r]<<" setting freq to "<<m[s[r]]<<endl;
-                if(m[s[r]]>=0) {
+        unordered_map<char, int> freq;
+        unordered_map<char, int> orifreq;
+        for(const auto& c : t) freq[c]++;
+        orifreq = freq;
+        int i = 0; int j = 0;
+        int m = s.size(); int n = t.size();
+        int count = n;
+        int reslen = 1e9;
+        int start = 0; int end = m-1;
+        while(j<m){
+            //cout<<j<<endl;
+            if(freq.find(s[j])!=freq.end()){
+                //cout<<"found "<<endl;
+                freq[s[j]]--;
+                if(freq[s[j]]>=0){
                     count--;
-                    //cout<<"decrementing count. set count to "<<count<<endl;
                 }
             }
             while(count==0){
-                //update min
-                if(mini>(r-l+1)) {
-                    mini = (r-l+1);
-                    minl = l;
-                    minr = r;
+                if(reslen>(j-i+1)){
+                    reslen = j-i+1;
+                    start = i; end = j;
                 }
-                if(m.find(s[l])!=m.end()){
-                    if(m[s[l]]==0) {
-                        m[s[l]]++;
-                        //cout<<"found "<<s[l]<<" setting freq to "<<m[s[l]]<<endl;
+                if(freq.find(s[i])!=freq.end()){
+                    freq[s[i]]++;
+                    if(freq[s[i]]>0){
                         count++;
-                        //cout<<"incrementing count. set count to "<<count<<endl;
-                    }else 
-                        m[s[l]]++;
+                    }
+                    //if(freq[s[i]]==1) count++;
                 }
-                l++;
+                i++;
             }
-            r++;
+            //cout<<count<<endl;
+            //for(const auto& [c, f] : freq) cout<<c<<" "<<f<<endl;
+            j++;
         }
-        return mini==numeric_limits<int>::max()?"":s.substr(minl, (minr-minl+1));
+        return reslen==1e9?"":s.substr(start, end-start+1);
     }
 };
