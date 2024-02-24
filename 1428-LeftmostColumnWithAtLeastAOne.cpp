615436e4fcea9e962bf1a36e3dbcc8f37aae92d2
rranjik <43015276+rranjik@users.noreply.github.com>
2024-02-23 23:41:23 -0800

    Feb 23

--- 1428-LeftmostColumnWithAtLeastAOne.cpp
+++ 1428-LeftmostColumnWithAtLeastAOne.cpp
@@ -3,49 +3,35 @@
  * // You should not implement it, or speculate about its implementation
  * class BinaryMatrix {
  *   public:
- *     int get(int x, int y);
+ *     int get(int row, int col);
  *     vector<int> dimensions();
  * };
  */
 
 class Solution {
 public:
-    int firstOneInRow(int row, int to, BinaryMatrix& b){
-        if(to<n){
-            auto valto = b.get(row, to);
-            if(!valto) return to+1;
-        }
-        int left = 0;
-        int right = to;
-        while(left<=right){
-            int mid = left+(right-left)/2;
-            cout<<"left = "<<left<<"; right = "<<right<<" mid = "<<mid<<endl;
-            auto val = b.get(row, mid);
-            if(mid==n-1&&val) return mid;
-            if(mid==n-1&&!val) return n+1;
-            auto next = b.get(row, mid+1);
-            if(mid==0&&!val&&next) return mid+1;
-            if(mid==0&&val) return mid;
-            if(!val&&next) return mid+1;
-            if(val){
-                right = mid-1;
+    int bsearch(BinaryMatrix& bim, int r, int n){
+        int lo = 0; int hi = n-1;
+        int res = -1;
+        while(lo<=hi){
+            auto m = (lo+hi)/2;
+            if(bim.get(r, m)==1){
+                res = m;
+                hi = m-1;
             }else{
-                left = mid+1;
+                lo = m+1;
             }
         }
-        return n+1;
+        return res==-1?n:res;
     }
-    int m;
-    int n;
-    int leftMostColumnWithOne(BinaryMatrix &b) {
-        auto dim = b.dimensions();
-        m = dim[0]; n = dim[1];
-        int res = n+1;
-        for(int i = 0; i<m; i++){
-            auto first1InRow = firstOneInRow(i, min(res, n-1), b);
-            cout<<"first one in row = "<<i<<" is "<< first1InRow<<endl;
-            res = min(res, first1InRow);
-        }
-        return res>=n?-1:res;
+    int leftMostColumnWithOne(BinaryMatrix &bim) {
+       auto v = bim.dimensions();
+       auto m = v[0]; auto n = v[1]; 
+       int res = n;
+       for(int i = 0; i<m; i++){
+           auto f = bsearch(bim, i, n);
+           res = min(res, f); 
+       }
+       return res==n?-1:res;
     }
 };
