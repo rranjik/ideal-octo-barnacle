f76c03ca162d13badeb0446e17efa9853407a875
rranjik <43015276+rranjik@users.noreply.github.com>
2024-03-01 23:04:50 -0800

    1239-MaximumLengthOfAConcatenatedStringWithUniqueCharacters.cpp

--- 1239-MaximumLengthOfAConcatenatedStringWithUniqueCharacters.cpp
+++ 1239-MaximumLengthOfAConcatenatedStringWithUniqueCharacters.cpp
@@ -1,43 +1,34 @@
 class Solution {
 public:
-    void dp(bitset<26> here, int i, int c, int& res, 
-           const vector<bitset<26>>& v){
-        //cout<<"i = "<<i<<"; c = "<<c
-            //<<"; res = "<<res<<"; here "<<here<<endl;
-        if(i==v.size()){
-            res = max(res, c);
-            return;
-        }
-        if(!(here&v[i]).any()){
-            dp(here|v[i], i+1, c+a[i].length(), res, v);
-        }
-        dp(here, i+1, c, res, v);
-    }
-    vector<string> a;
     int maxLength(vector<string>& arr) {
-        a = arr;
-        vector<bitset<26>> tarr;
-        vector<bool> tr(arr.size());
-        for(int i = 0; i<arr.size(); i++){
-            auto nn = bitset<26>();
-            vector<int> f(26, 0);
-            tarr.push_back(nn);
-            for(const auto& c : arr[i]){
-                if(f[c-'a']) {tr[i] = true;}
-                tarr[i][c-'a'] = 1;
-                f[c-'a']++;
-            }
-        }
-        for(int i = tr.size()-1; i>=0; i--){
-            if(tr[i]){
-                tarr.erase(tarr.begin()+i);
-                arr.erase(arr.begin()+i);
-            }
-        }
-        a = arr;
+        int n = arr.size();
+        int m = 1<<n;
+        //vector<int> hitm(n);
+        //for(int i = 0; i<arr.size(); i++){
+        //    auto a = arr[i];
+        //    for(const auto& c : a){
+        //        hitm[i]|=(c-'a');
+        //    }
+        //}
         int res = 0;
-        bitset<26> n;
-        dp(n, 0, 0, res, tarr);
+        for(int i = 0; i<m; i++){
+            vector<int> f(26);
+            int rres = 0;
+            for(int j = 0; j<n; j++){
+                if((i&(1<<j))==0)continue;
+                rres+=arr[j].size();
+                for(const auto& c : arr[j]){
+                    f[c-'a']++;
+                    if(f[c-'a']>=2) break;
+                }
+            }
+            bool okay = true;
+            for(const auto& x : f) if(x>1) okay = false;
+            //cout<<i<<": ";
+            //for(const auto& x : f) cout<<x<<"."; cout<<endl;
+            if(okay)
+            res = max(res, rres);
+        }
         return res;
     }
 };
