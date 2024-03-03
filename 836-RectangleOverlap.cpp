41044dbe01a6a36fe4778d38791b70302631ed02
rranjik <43015276+rranjik@users.noreply.github.com>
2024-03-02 22:12:47 -0800

    Mar 2

--- 836-RectangleOverlap.cpp
+++ 836-RectangleOverlap.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
-        auto ires = rec2[0]>=rec1[2] || //rec2 is to the right
-            rec2[2]<=rec1[0] || //rec2 is to the left
-            rec2[3]<=rec1[1] || //rec2 is at the bottom
-            rec2[1]>=rec1[3]; //rec2 is at the top
-        return !ires;
+        int llx1 = rec1[0];
+        int lly1 = rec1[1];
+        int urx1 = rec1[2];
+        int ury1 = rec1[3];
+        
+        int llx2 = rec2[0];
+        int lly2 = rec2[1];
+        int urx2 = rec2[2];
+        int ury2 = rec2[3];
+        
+        return !((llx2>=urx1) || (lly2>=ury1) || (urx2<=llx1) || (ury2<=lly1));
     }
 };
