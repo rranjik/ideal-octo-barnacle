ab13fe4381ea8d759eb962bfdc71e238ff445f5d
rranjik <43015276+rranjik@users.noreply.github.com>
2024-03-06 23:31:32 -0800

    Mar 6

--- 227-BasicCalculatorII.cpp
+++ 227-BasicCalculatorII.cpp
@@ -1,30 +1,87 @@
 class Solution {
 public:
-    int calculate(string s) {
-        long res = 0, num = 0, n = s.size();
-        char op = '+';
-        stack<int> st;
-        for (int i = 0; i < n; ++i) {
-            cout<<s[i]<<endl;
-            if (s[i] >= '0') {
-                num = num * 10 + s[i] - '0';
+    unordered_map<char, int> pred = {
+        {')', -1},
+        {'+', 0},
+        {'-', 0},
+        {'*', 1},
+        {'/', 1},
+    };
+    string trim(string s){
+        string res;
+        for(int i = 0; i<s.size(); i++){
+            if(s[i]!=' '){
+                res+=s[i];
             }
-            if ((s[i] < '0' && s[i] != ' ') || i == n - 1) {
-                if (op == '+') { cout<<"pusing "<<num<<endl; st.push(num);}
-                if (op == '-') { cout<<"pusing "<<-num<<endl; st.push(-num);}
-                if (op == '*' || op == '/') {
-                    int tmp = (op == '*') ? st.top() * num : st.top() / num;
-                    st.pop();cout<<"pusing "<<tmp<<endl;
-                    st.push(tmp);
-                }
-                op = s[i];
-                num = 0;
-            } 
-        }
-        while (!st.empty()) {
-            res += st.top();
-            st.pop();
         }
         return res;
+    }
+    string remunaryminus(string s){
+        for(int i = 0; i<s.size(); i++){
+            if(s[i]=='-'&&!i){
+                s = '0'+s;
+            }
+        }
+        return s;
+    }
+    int calculate(string s) {
+        s = trim(s);
+        s = remunaryminus(s);
+        s = "("+s+")";
+        cout<<s<<endl;
+        stack<char> opstack;
+        stack<int> numstack;
+        int i = 0;
+        while(i<s.size()){
+            auto c = s[i];
+            if(c=='('){
+                opstack.push(c);
+                i++;
+                continue;
+            }
+            if((c>='0')&&(c<='9')){
+                int num = 0;               
+                while(i<s.size()&&(s[i]>='0')&&(s[i]<='9')){
+                    num = num*10+(s[i]-'0');
+                    i++;
+                }
+                cout<<"num = "<<num<<endl;
+                numstack.push(num);
+            }else {
+                cout<<c<<endl;
+                while(!opstack.empty()&&opstack.top()!='('&&
+                pred[c]<=pred[opstack.top()]){
+                    auto n2 = numstack.top(); numstack.pop();
+                    auto n1 = numstack.top(); numstack.pop();
+                    auto op = opstack.top(); opstack.pop();
+                    auto x = eval(op, n1, n2);
+                    cout<<n1<<op<<n2<<" = "<<x<<endl;
+                    numstack.push(x);
+                }
+                if(c==')'){
+                    opstack.pop();
+                }else{
+                    cout<<"pushed "<<c<<endl;
+                    opstack.push(c);
+                }
+                i++;
+            }
+        }
+        return numstack.top();
+    }
+    int eval(char c, int n1, int n2){
+        switch(c){
+            case '+':
+                return n1+n2;
+            case '-':
+                return n1-n2;
+            case '/':
+                return n1/n2;
+            case '*':
+                return n1*n2;
+            default:
+                return 0;
+        }
+        return 0;
     }
 };
