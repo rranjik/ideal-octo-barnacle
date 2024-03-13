34992e3a654f711f8cf5af720328b032502bdeef
rranjik <43015276+rranjik@users.noreply.github.com>
2024-03-12 19:15:04 -0700

    Mar 12

--- 1443-MinimumTimeToCollectAllApplesInATree.cpp
+++ 1443-MinimumTimeToCollectAllApplesInATree.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> adjl;
-    bool dfs(int src, int& res){
-        bool subTreeHasApples = false;
-        for(const auto& c : adjl[src]){
-            auto thisChildHasApple = dfs(c, res);
-            if(thisChildHasApple) res+=2;
-            subTreeHasApples|= thisChildHasApple;
+    pair<int, bool> dfs(int src, int par, 
+        const vector<vector<int>>& adjl, 
+        const vector<bool>& has){
+        pair<int, bool> res = {0, false};
+        if(has[src]) {
+            res.second = true;
         }
-        return subTreeHasApples|has[src];
-    }
-    vector<bool> has;
-    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
-        has = hasApple;
-        adjl = vector<vector<int>>(edges.size()+1);
-        for(int i = 0; i<edges.size(); i++){
-            adjl[edges[i][0]].push_back(edges[i][1]);
+        for(const auto& v: adjl[src]){
+            if(v==par) continue;
+            auto [vstep, vapple] = dfs(v, src, adjl, has);
+            res.first += vstep;
+            res.second |= vapple;
         }
-        int i = 0;
-        for(const auto& children : adjl){
-            cout<<"I'm "<<i<<". I can reach ";
-            for(const auto& c : children) cout<<c<<" ";
-            cout<<endl;
-        }
-        int res{};
-        dfs(0, res);
+        if(res.second) res.first+=2;
+        cout<<src<<" returns "<<res.first<<endl;
         return res;
+    }
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
+        //v = vector<bool>(n);
+        vector<vector<int>> adjl(n);
+        for(const auto& e : edges){
+            auto u = e[0]; auto v = e[1];
+            adjl[u].push_back(v);
+            adjl[v].push_back(u);
+        }
+        auto res = dfs(0, -1, adjl, hasApple);
+        return res.first?res.first-2:0;
     }
 };
