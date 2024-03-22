3825340c8a62ed890ce15d6ab4bfef311d9de8ae
rranjik <43015276+rranjik@users.noreply.github.com>
2024-03-21 19:37:54 -0700

    2094-Finding3DigitEvenNumbers.cpp

--- 2094-Finding3DigitEvenNumbers.cpp
+++ 2094-Finding3DigitEvenNumbers.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
-    vector<int> findEvenNumbers(vector<int>& digits) {
-        vector<int> df(10);
-        for(const auto& d : digits) df[d]++;
-        vector<int> res;
-        for(int i = 100; i<=999; i++){
-            if(i%2) continue;
-            auto s = to_string(i);
-            auto dff = df;
-            int okay = true;
-            for(const auto& c : s){
-                int d = c-'0';
-                dff[d]--;
-                if(dff[d]<0) {
-                    okay = false;
-                    break;
-                }
-            }
-            if(okay) res.push_back(i);
+    void dfs(int j, int c, vector<int>& res, vector<int>& f){
+        cout<<"f.size()="<<f.size()<<endl;
+        for(const auto& n : f){
+            cout<<n<<" ";
         }
+        cout<<endl;
+        cout<<"j = "<<j<<" c = "<<c<<endl;
+        if(j==3){
+            if(c%2==0&&c>=100&&c<=999){
+                res.push_back(c);
+            }
+            return;
+        }
+        for(int i = 0; i<f.size(); i++){
+            if(f[i]>0){
+                int temp = c;
+                c*=10;
+                c+=i;
+                f[i]--;
+                dfs(j+1, c, res, f);
+                f[i]++;
+                c = temp;
+            }
+        }
+    }
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        vector<int> f(10);
+        for(const auto& d : digits){
+            f[d]++;
+        }
+        vector<int> res;
+        dfs(0, 0, res, f);
         return res;
     }
 };
