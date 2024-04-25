2545cd99d7f8fe14845101972b73ffe0e9660e38
rranjik <43015276+rranjik@users.noreply.github.com>
2024-04-24 17:51:51 -0700

    Apr 24

--- 2177-FindThreeConsecutiveIntegersThatSumToAGivenNumber.cpp
+++ 2177-FindThreeConsecutiveIntegersThatSumToAGivenNumber.cpp
@@ -1,8 +1,22 @@
 class Solution {
 public:
     vector<long long> sumOfThree(long long num) {
-        if(num%3) return {};
-        long long x = (num+3)/3;
-        return {x-2, x-1, x};
+        if(!num) return {-1, 0, 1};
+        long long l = 0; 
+        long long r = num-1;
+        while(l<=r){
+            cout<<"l = "<<l<<" r = "<<r<<endl;
+            long long m = l+(r-l)/2;
+            auto c = m + (m-1) + (m-2);
+            if(c==num){
+                return {(m-2), (m-1), m};
+            }
+            if(c<num){
+                l = m+1;
+            }else{
+                r = m-1;
+            }
+        }
+        return {};
     }
 };
