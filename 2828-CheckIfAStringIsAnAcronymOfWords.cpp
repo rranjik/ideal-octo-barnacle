2d03fe1f267b22e5d596c8228a100a5ac39e3671
rranjik <43015276+rranjik@users.noreply.github.com>
2024-07-14 23:50:17 -0700

    2828-CheckIfAStringIsAnAcronymOfWords.cpp

---
+++ 2828-CheckIfAStringIsAnAcronymOfWords.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isAcronym(vector<string>& words, string s) {
+        if(words.size()!=s.size())return false;
+        int n = words.size();
+        for(int i = 0; i<n; i++){
+            if(words[i][0]!=s[i]) return false;
+        }
+        return true;
+    }
+};
