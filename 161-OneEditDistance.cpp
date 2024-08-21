afac68d1c77f4836f72146c7187db5d7a012ab5a
rranjik <43015276+rranjik@users.noreply.github.com>
2024-08-20 22:57:28 -0700

    161-OneEditDistance.cpp

---
+++ 161-OneEditDistance.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isOneEditDistance(string s, string t) {
+        for(int i = 0; i<min(s.size(), t.size()); i++){
+            if(s[i]!=t[i]){
+                if(s.size()==t.size()){
+                    return s.substr(i+1) == t.substr(i+1);
+                }else if(s.size()<t.size()){
+                    return s.substr(i) == t.substr(i+1);
+                }else {
+                    return s.substr(i+1) == t.substr(i);
+                }
+            }
+        }
+        return abs((int)((int)s.size()-(int)t.size()))==1;
+    }
+};
