5e5a32340e40b0c5fd681e6d13c0a5c6c0125024
rranjik <43015276+rranjik@users.noreply.github.com>
2024-09-24 15:47:24 -0700

    2027-MinimumMovesToConvertString.cpp

---
+++ 2027-MinimumMovesToConvertString.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumMoves(string s) {
+        int res = 0;
+        for(int i = 0; i<s.size(); i++){
+            if(s[i]=='X'){
+                res++;
+                i+=2;
+            }
+        }
+        return res;
+    }
+};
