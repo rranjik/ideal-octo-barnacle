1304e0ed0ac3da7f010e02d03af5f3c1bfb17f65
rranjik <43015276+rranjik@users.noreply.github.com>
2024-10-02 18:52:18 -0700

    907-SumOfSubarrayMinimums.cpp

--- 907-SumOfSubarrayMinimums.cpp
+++ 907-SumOfSubarrayMinimums.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    int sumSubarrayMins(vector<int>& A) {
-        vector<int> left(A.size());
-        vector<int> right(A.size());
-        stack<int> ple;
-        stack<int> nle;
-        for(int i = 0; i<A.size(); i++) left[i]=(i+1);
-        for(int i = 0; i<A.size(); i++) right[i] = A.size()-i;
-        for(int i = 0; i<A.size(); i++){
-            while(!ple.empty()&&A[ple.top()]>A[i]){
-                ple.pop();
-            }
-            left[i]=(ple.empty())?i+1:i-ple.top();
-            ple.push(i);
-            while(!nle.empty()&&A[nle.top()]>A[i]){
-                right[nle.top()] = i-nle.top();
-                nle.pop();
-            }
-            nle.push(i);
+    int sumSubarrayMins(vector<int>& a) {
+        int n = a.size();
+        vector<int> ple(n, -1); vector<int> nle(n, n);
+        stack<int> s;
+        for(int i = 0; i<n; i++){
+            while(!s.empty()&&a[s.top()]>a[i]) s.pop();
+            if(!s.empty()) ple[i] = s.top();
+            s.push(i);
         }
-        int res = 0; 
-        int mod = 1e9+7;
-        for(int i = 0; i<A.size(); i++){
-            res = (res + A[i]*left[i]*right[i])%mod;
+        s = stack<int>();
+        for(int i = n-1; i>=0; i--){
+            while(!s.empty()&&a[s.top()]>=a[i]) s.pop();
+            if(!s.empty()) nle[i] = s.top();
+            s.push(i);
+        }
+        for(const auto& x : ple) cout<<x<<" "; cout<<endl;
+        for(const auto& x : nle) cout<<x<<" "; cout<<endl;
+        long long res = 0;
+        for(int i = 0; i<n; i++){
+            res+=((long long)(i-(ple[i]+1)+1)*(long long)(nle[i]-1-i+1)*(long long)a[i]);
+            res%=((int)1e9+7);
         }
         return res;
     }
