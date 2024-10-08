d8562d8ef5163ec3467cdfec624e04e34987f6c8
rranjik <43015276+rranjik@users.noreply.github.com>
2024-10-07 20:27:05 -0700

    Oct 7

--- maximumsubarray.cpp
+++ maximumsubarray.cpp
@@ -1,26 +1,18 @@
 class Solution {
- public :
-    vector<int> n;
-    unordered_map<int, int> m;
-    int dp(int i){
-        if(m.find(i)!=m.end()){
-            return m[i];
+public:
+    int maxSubArray(vector<int>& a) {
+        if(a.size()==0) return 0;
+        if(a.size()==1) return a[0];
+        vector<int> ps = a;
+        for(int i = 1; i<a.size(); i++){
+            ps[i] = ps[i]+ps[i-1];
         }
-        if(i==0){
-            m.insert({i,n[0]});
-            return m[i];
-        }else{
-            m.insert({i, max(dp(i-1)+n[i], n[i])});
-            return m[i];
+        int prevmin = 0;
+        int res = ps[0]-prevmin;
+        for(int i = 1; i<a.size(); i++){
+            prevmin = min(prevmin, ps[i-1]);
+            res = max(res, ps[i]-prevmin);
         }
-    }
-    int maxSubArray (vector < int > & nums) {
-        n = nums;
-        int mx = numeric_limits<int>::min();
-        dp(nums.size()-1);
-        for(auto it : m){
-            mx = max(mx, it.second);
-        }
-        return mx;
+        return res;
     }
 };
