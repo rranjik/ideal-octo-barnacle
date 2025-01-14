6c378de7de095e1463121338896c54e10d9d362a
rranjik <43015276+rranjik@users.noreply.github.com>
2025-01-13 22:52:24 -0800

    Jan 13

--- 45-JumpGameII.cpp
+++ 45-JumpGameII.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
-    int jump(vector<int>& nums) {
-        int maximum_reach = 0;
-        int end_of_element_i_reach = 0;
-        int res = 0;
-        for(int i = 0; i<nums.size()-1; i++){
-            maximum_reach = max(maximum_reach, i+nums[i]);
-            if(i==end_of_element_i_reach){
-                res++;
-                end_of_element_i_reach = maximum_reach;
-            }
+    vector<int> a;
+    unordered_map<int, int> m;
+    int dp(int i){
+        if(m.find(i)!=m.end()) return m[i];
+        if(i>=n-1) return 0;
+        int res = 1e9;
+        int x = a[i];
+        for(int j = 1; j<= a[i]; j++){
+            res = min(res, dp(i+j)+1);
         }
-        return res;
+        return m[i] = res;
+    }
+    int n;
+    int jump(vector<int>& nums) {
+        a = nums;
+        n = a.size();
+        return dp(0);
     }
 };
