ee016afb5164e843c64fcb51ccd1e9a5b0416ea1
rranjik <43015276+rranjik@users.noreply.github.com>
2025-01-20 23:28:55 -0800

    Jan 20

--- 1248-CountNumberOfNiceSubarrays.cpp
+++ 1248-CountNumberOfNiceSubarrays.cpp
@@ -1,19 +1,47 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        int i{}, count{}, res{};
-        for(int j = 0; j<nums.size(); j++){
-            if(nums[j]%2){
-                k--;
-                count = 0;
+        int n = nums.size();
+       vector<int>pf(n,0);
+       int p= 0;
+       
+       for(int i = 0;i<nums.size();i++)
+       {
+            if(nums[i]%2!=0)
+            {
+               pf[i]=p+1;
+               p= pf[i];
             }
-            while(!k){
-                if(nums[i]%2) k++;
-                count++;
-                i++;
+            else
+            {
+               pf[i]= p; 
             }
-            res+=count;
-        }
-        return res;
+            
+       }
+       for(auto x: pf)
+       {
+         cout<< x <<" ";
+       }
+       cout<<endl;
+       map<int,int>mp;
+       mp[0]++;
+       int count=0;
+       for(int i=0;i<pf.size();i++)
+       {
+            int to_find = pf[i]-k;
+            cout<<to_find<<endl;
+            if(mp.find(to_find)!=mp.end())
+            {
+                count+= mp[to_find];
+                
+            }
+            
+                mp[pf[i]]++;
+            
+       }
+       
+       return count;
+
+
     }
 };
