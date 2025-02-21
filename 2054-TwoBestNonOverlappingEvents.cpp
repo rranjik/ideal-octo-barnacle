8d78a3f035c61d882b65a5e1a6c4b7b345559a2e
rranjik <43015276+rranjik@users.noreply.github.com>
2025-02-20 21:11:35 -0800

    Update 2054-TwoBestNonOverlappingEvents.cpp

--- 2054-TwoBestNonOverlappingEvents.cpp
+++ 2054-TwoBestNonOverlappingEvents.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int maxTwoEvents(vector<vector<int>>& events) {
-        int n = events.size(); 
-        sort(events.begin(), events.end());
-        int res = 0;
-        int maxx = 0;
-        map<int, int> mp;
+    int maxTwoEvents(vector<vector<int>>& e) {
+        int n = e.size();
+        sort(e.begin(), e.end());
         int i = n-1;
+        int res = 0;
+        int maxx = -1;
+        map<int, int> m;
         while(i>=0){
-            auto it = mp.upper_bound(events[i][1]);
-            maxx = max(maxx, events[i][2]);
-            mp[events[i][0]] = maxx;
-            res = max(maxx, res);
-            if(it!=mp.end()){
-                res = max(res, events[i][2]+it->second);
+            auto st = e[i][0]; auto en = e[i][1];
+            auto ct = e[i][2];
+            auto it = m.upper_bound(en);
+            res = max(res, ct);
+            maxx = max(maxx, ct);
+            m[st] = maxx;
+            if(it!=m.end()){
+                res = max(res, ct+it->second);
             }
             i--;
         }
